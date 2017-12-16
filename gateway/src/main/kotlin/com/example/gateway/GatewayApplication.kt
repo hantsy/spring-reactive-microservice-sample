91874a6f2b685ef0fa23763888a8cd2c4791ae0f
hantsy <hantsy@gmail.com>
2017-12-16 16:42:12 +0800

    added gateway codes

--- gateway/src/main/kotlin/com/example/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/example/gateway/GatewayApplication.kt
@@ -1,11 +1,135 @@
 package com.example.gateway
 
+import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+import org.springframework.cloud.gateway.filter.factory.RequestRateLimiterGatewayFilterFactory
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.cloud.netflix.hystrix.HystrixCommands
+import org.springframework.context.support.beans
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class GatewayApplication
 
 fun main(args: Array<String>) {
-    runApplication<GatewayApplication>(*args)
+    runApplication<GatewayApplication>(*args) {
+        beans {
+            bean {
+                DiscoveryClientRouteDefinitionLocator(ref())
+            }
+//            bean {
+//                MapReactiveUserDetailsService(
+//                        User.withDefaultPasswordEncoder()
+//                                .username("user")
+//                                .roles("USER")
+//                                .password("pw")
+//                                .build())
+//            }
+//            bean {
+//                //@formatter:off
+//                val http = ref<ServerHttpSecurity>()
+//                http
+//                        .csrf().disable()
+//                        .httpBasic()
+//                        .and()
+//                        .authorizeExchange()
+//                        .pathMatchers("/proxy").authenticated()
+//                        .anyExchange().permitAll()
+//                        .and()
+//                        .build()
+//                //@formatter:on
+//            }
+            bean {
+                val builder = ref<RouteLocatorBuilder>()
+                builder
+                        .routes {
+                            route {
+                                path("/user")
+                                uri("lb://auth-service/user")
+                            }
+
+                            route {
+                                val rl = ref<RequestRateLimiterGatewayFilterFactory>()
+                                        .apply(RedisRateLimiter.args(2, 4))
+                                path("/posts")
+                                filters {
+                                    filter(rl)
+                                }
+                                uri("lb://post-service/posts")
+                            }
+                        }
+            }
+            bean {
+                WebClient.builder().filter(ref<LoadBalancerExchangeFilterFunction>()).build()
+            }
+            bean {
+                router {
+                    val client = ref<WebClient>()
+
+                    GET("/posts/{slug}/favorites") {
+
+                        val favorites: Publisher<String> = client
+                                .get()
+                                .uri("http://favorite-service/posts/{slug}/favorites", it.pathVariable("slug"))
+                                .retrieve()
+                                .bodyToFlux(ParameterizedTypeReference.forType(String::class.java))
+
+
+                        val cb = HystrixCommands
+                                .from(favorites)
+                                .commandName("posts-favorites")
+                                .fallback(Flux.just("favorited users!!"))
+                                .eager()
+                                .build()
+
+                        ServerResponse.ok().body(cb)
+                    }
+
+                    GET("/user/favorites") {
+
+                        val favorites: Publisher<FavoritedPost> = client
+                                .get()
+                                .uri("http://favorite-service/users/{username}/favorites", it.principal().block()?.name)
+                                .retrieve()
+                                .bodyToFlux(String::class.java)
+                                .flatMap {
+                                    client
+                                            .get()
+                                            .uri("http://post-service/posts/{slug}/", it)
+                                            .retrieve()
+                                            .bodyToFlux(Post::class.java)
+                                            .map {
+                                                (_, title, slug, _, createdDate) -> FavoritedPost(title, slug, createdDate)
+                                            }
+                                }
+
+
+                        val cb = HystrixCommands
+                                .from(favorites)
+                                .commandName("posts-favorites")
+                                .fallback(Flux.just(FavoritedPost("Loading error", "not_loaded", LocalDateTime.now())))
+                                .eager()
+                                .build()
+
+                        ServerResponse.ok().body(cb)
+                    }
+                }
+            }
+        }
+    }
 }
+
+data class Post(var id: Long, var title: String, var slug: String, var content: String, var createdData: LocalDateTime)
+data class FavoritedPost(var title: String, var slug: String, var createdDate: LocalDateTime)
